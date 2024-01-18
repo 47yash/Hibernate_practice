d985619b1109bb79a0f5459b2f223f03a2c6d6b9
47yash <144767088+47yash@users.noreply.github.com>
2024-01-18 11:25:07 +0530

    Add files via upload

---
+++ springBootCrud/src/main/java/com/demo/springBootMVC.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springBootMVC {
+
+	public static void main(String[] args) {
+		
+			//it returns ApplicationContext
+			SpringApplication.run(springBootMVC.class,args);
+		}
+}
+
+
